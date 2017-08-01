89d97c44f48e4852c11fc959251579653566765c
宇成童鞋 <646928503@qq.com>
2017-08-01 13:59:31 +0800

    optimize

--- app/src/main/java/com/david/zhihudaily/zhihu/NewsListModel.java
+++ app/src/main/java/com/david/zhihudaily/zhihu/NewsListModel.java
@@ -4,14 +4,9 @@ import java.util.ArrayList;
 
 public class NewsListModel {
 
-    private String date;
     private ArrayList<NewsModel> stories;
 
     public NewsListModel() {
-    }
-
-    public String getDate() {
-        return date;
     }
 
     public ArrayList<NewsModel> getStories() {
